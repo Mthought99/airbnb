6d56c03b1677fc61b2f38c1b261a0538ff4e2145
seyimichael <shodiya508@gmail.com>
2022-09-15 00:24:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Navbar from "./component/Navbar"
+import Hero from "./component/Hero"
+import Card from "./component/Card"
+import data from "./data"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+    const cards = data.map(item => {
+      return (
+          <Card 
+            key={item.id}
+            {...item}
+
+            //item={item}
+            // img={item.coverImg}
+            // rating={item.stats.rating}
+            // reviewCount={item.stats.reviewCount}
+            // location={item.location}
+            // title={item.title}
+            // price={item.price}
+            // openSpots={item.openSpots}
+          />
+        )
+    })
+
+    return (
+      <div>
+          <Navbar />
+          <Hero />
+          <section className="cards-list">
+            {cards}
+          </section>  
+      </div>
+    )
+}   
